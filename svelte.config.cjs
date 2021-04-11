ff63e8dbf7d0ce830b89d8050d710cb11e6c9ff2
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-04-11 17:11:50 +0100

    Remove all auto-encrypt-localhost references from SvelteKit config

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,14 +1,5 @@
-const os = require('os')
-const fs = require('fs')
-const path = require('path')
 const static = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
-
-// Note: the local TLS certificates will have been created at
-// the post-install stage of npm install.
-const certDirectory = path.join(os.homedir(), '.small-tech.org', 'auto-encrypt-localhost')
-const cert = fs.readFileSync(path.join(certDirectory, 'localhost.pem'))
-const key = fs.readFileSync(path.join(certDirectory, 'localhost-key.pem'))
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
